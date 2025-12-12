50b4fd0349a0d641a1b34fcc063c1674d2369e88
Abrar <abrar_algh@outlook.sa>
2025-12-12 13:40:07 +0300

    update

--- AstronomyBeyondLearning/AstronomyBeyondLearning/settings.py
+++ AstronomyBeyondLearning/AstronomyBeyondLearning/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'main.middleware.DisableBrowserCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'AstronomyBeyondLearning.urls'
